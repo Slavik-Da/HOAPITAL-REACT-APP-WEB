17d647bb633594a4c7156e0076317434ed5dfaf6
Slavik-Da <BuHTOBO4KuH@gmail.com>
2021-07-27 16:17:50 +0300

    Refactoring of stuff page, added active tabs style

--- src/pages/Stuff/Stuff.tsx
+++ src/pages/Stuff/Stuff.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import { StuffHeader } from './components/StuffHeader/StuffHeader';
 
 export const Stuff = () => {
     return (
-        <section className="page page_stuff">
-            <ul className= "tab-line">
-
-            </ul>
+        <section className="page-stuff">
+            <StuffHeader/>
         </section>
     )
 }
---
+++ src/pages/Stuff/components/StuffHeader/StuffHeader.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const StuffHeader = () => {
+    return (
+            <div className="pannel-header-line">
+                <div className= "tab-line">
+                    <div>Doctors</div>
+                    <div className="tab-line__active">Assistants</div>
+                    <div>Receptionists</div>
+                </div>
+                <button>Add new</button>
+            </div>
+    )
+}
