9549d8a4b78e93625167cdc63d6ea3d6c877c1be
Slavik-Da <BuHTOBO4KuH@gmail.com>
2021-07-28 19:22:16 +0300

    StuffPage css refactoring(include adaptiveFont)

--- src/pages/Stuff/components/StuffItems/StuffItems.tsx
+++ src/pages/Stuff/components/StuffItems/StuffItems.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const StuffItems = () => {
     return (
-        <div className='page-stuff stuff-items'>
-            Stuff Items
+        <div className='stuff-items'>
+            Stuff Items Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut voluptatibus illum rerum. Est repudiandae dolore, cumque voluptatum explicabo quasi odio voluptatem provident magnam in! Numquam voluptatum impedit reprehenderit vel modi.
         </div>
     )
 }
