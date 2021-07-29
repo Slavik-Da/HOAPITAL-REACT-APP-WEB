0e6bc6444b7af2592ac2af1f910b73ee4aafe012
Slavik-Da <BuHTOBO4KuH@gmail.com>
2021-07-29 16:24:57 +0300

    created files for universal component UserLine

---
+++ src/pages/Stuff/components/StuffItems/StuffItem/StuffItem.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export const StuffItem = () => {
+    return (
+        <div className='stuff-item'>
+            <UserLine/>
+        </div>
+    )
+}
--- src/pages/Stuff/components/StuffItems/StuffItems.tsx
+++ src/pages/Stuff/components/StuffItems/StuffItems.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { StuffItem } from "./StuffItem/StuffItem";
 
 export const StuffItems = () => {
     return (
         <div className='stuff-items'>
-            Stuff Items Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut voluptatibus illum rerum. Est repudiandae dolore, cumque voluptatum explicabo quasi odio voluptatem provident magnam in! Numquam voluptatum impedit reprehenderit vel modi.
+            <StuffItem/>
+            <StuffItem/>
+            <StuffItem/>
         </div>
     )
 }
