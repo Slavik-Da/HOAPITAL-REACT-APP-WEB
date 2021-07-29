ca5445a0a2c20c0b81dc173cd11f2feb40ceb72f
Артем Шарапанюк <artem.sharapanyk@hotemail.com>
2021-07-29 18:04:32 +0300

    fix some parts of AR-7

--- src/pages/Stuff/components/StuffHeader/StuffHeader.tsx
+++ src/pages/Stuff/components/StuffHeader/StuffHeader.tsx
@@ -6,9 +6,15 @@ export const StuffHeader = () => {
     return (
             <div className="pannel-header-line">
                 <div className= "tab-line">
-                    <div>Doctors</div>
-                    <div className="tab-line__active">Assistants</div>
-                    <div className="tab-line__active">Receptionists</div>
+                    <div className="tab-line__tab">
+                        Doctors
+                    </div>
+                    <div className="tab-line__tab">
+                        Assistants
+                    </div>
+                    <div className="tab-line__tab tab-line__tab_active">
+                        Receptionists
+                    </div>
                 </div>
                 <Button 
                     type= {EnumButtonsTypes.BIG_GREEN}
